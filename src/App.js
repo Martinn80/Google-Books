6daf58a3f8c8c801f14d64adef37ddaa189314a6
Martin Nguyen <ngumartin@gmail.com>
2020-03-17 21:08:10 -0700

    updated readme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+  const [book, setBook] = useState('')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Google Books</h1>
+      <input onChange={(e) => console.log(e.target.value)} className="form-control" type="text" />
+      <button className="btn btn-primary">Submit</button>
     </div>
   );
 }
